f78cb9838e6364faabade4cbcd361eb61e46dfc3
John Susek <john@johnsolo.net>
2023-03-23 21:32:30 -0500

    Latest

---
+++ LaunchToMainMenu.ps1
@@ -0,0 +1,33 @@
+﻿# Cyberpunk2077 launch script to skip final intro movie
+# Usage: powershell -ExecutionPolicy Bypass -File "Cyberpunk 2077 - Spacebar Pusher.ps1"
+
+# After launch this script will tap spacebar once every second
+# until $stopAfter many seconds have elapsed
+
+# Tweak this number to match your system
+# If this number is too low the intro won't be skipped
+# In this number is too high spacebar will keep being tapped in the main menu
+$stopAfter = 25
+
+$path = "C:\Program Files\Epic Games\Cyberpunk2077\bin\x64\Cyberpunk2077.exe"
+$args = "-modded --launcher-skip -skipStartScreen --windowed --width 2048 --height 1152"
+
+# Kill previous instances running
+taskkill /im Cyberpunk2077.exe
+taskkill /f /im Cyberpunk2077.exe
+taskkill /im scc.exe
+taskkill /f /im scc.exe
+
+psexec -i 1 "$path $args"
+
+Start-Sleep 1
+
+$title = "Cyberpunk 2077 (C) 2020 by CD Projekt RED"
+$shell = New-Object -ComObject wscript.shell
+
+for ($idx = 1; $idx -lt $stopAfter; $idx++)
+{
+  $_ = $shell.AppActivate($title)
+  $_ = $shell.SendKeys(' ')
+  Start-Sleep 1
+}
--- src/HttpRequest.cpp
+++ src/HttpRequest.cpp
@@ -1,9 +1,13 @@
+#include <Poco/JSON/JSON.h>
+#include <Poco/JSON/Object.h>
+#include <Poco/JSON/Parser.h>
 #include <RED4ext/RED4ext.hpp>
-#include <cpr/cpr.h>
+#include <iostream>
 
 namespace HttpRequest {
-using namespace cpr;
+using namespace std;
 using namespace RED4ext;
+using namespace Poco::JSON;
 
 struct HttpRequest : IScriptable {
   CClass *GetNativeType();
@@ -12,6 +16,27 @@ struct HttpRequest : IScriptable {
 TTypedClass<HttpRequest> httpRequest("HttpRequest");
 
 CClass *HttpRequest::GetNativeType() { return &httpRequest; }
+
+void parseJson() {
+  std::string json = "{ \"test\" : { \"property\" : \"value\" } }";
+  Parser parser;
+  auto result = parser.parse(json);
+
+  // use pointers to avoid copying
+  Object::Ptr object = result.extract<Object::Ptr>();
+  auto test = object->get("test"); // holds { "property" : "value" }
+
+  Object::Ptr subObject = test.extract<Object::Ptr>();
+  test = subObject->get("property");
+  std::string val = test.toString(); // val holds "value"
+
+  // copy/convert to Poco::DynamicStruct
+  Poco::DynamicStruct ds = *object;
+  auto val2 = ds["test"]["property"]; // val holds "value"
+
+  cout << val << endl;
+  cout << val2.toString() << endl;
+}
 
 void open(IScriptable *aContext, CStackFrame *aFrame, CString *aOut,
           int64_t a4) {
@@ -24,32 +49,54 @@ void open(IScriptable *aContext, CStackFrame *aFrame, CString *aOut,
   GetParameter(aFrame, &url);
   Handle<IScriptable> handler;
   GetParameter(aFrame, &handler);
+  Handle<IScriptable> schema;
+  GetParameter(aFrame, &schema);
+
+  parseJson();
+
+  // auto instance = schema.instance;
+  // auto instanceType = instance->GetType();
+
+  // RED4ext::DynArray<RED4ext::CProperty *> props;
+  // instanceType->GetProperties(props);
+
+  // for (auto &prop : props) {
+  //   auto name = prop->name.ToString();
+  //   CName propType = prop->type->GetName();
+  //   auto type = propType.ToString();
+  // Get key with `name` from JSON and check if it is `type`
+  // then SetValue on the prop
+  // }
 
   aFrame->code++;
 
-  auto onWrite = [handler](std::string data, intptr_t userdata) -> bool {
-    handler.instance->ExecuteFunction(CName("OnWrite"), CString(data.data()));
-    return true;
-  };
+  // auto onWrite = [handler](string data, intptr_t userdata) -> bool {
+  //   handler.instance->ExecuteFunction(CName("OnWrite"),
+  //   CString(data.data())); return true;
+  // };
 
-  auto onHeader = [handler](std::string data, intptr_t userdata) -> bool {
-    handler.instance->ExecuteFunction(CName("OnHeader"), CString(data.data()));
-    return true;
-  };
+  // auto onHeader = [handler](string data, intptr_t userdata) -> bool {
+  //   handler.instance->ExecuteFunction(CName("OnHeader"),
+  //   CString(data.data())); return true;
+  // };
 
-  auto onProgress = [handler](cpr_off_t downloadTotal, cpr_off_t downloadNow,
-                              cpr_off_t uploadTotal, cpr_off_t uploadNow,
-                              intptr_t userdata) -> bool {
-    handler.instance->ExecuteFunction(CName("OnProgress"), downloadTotal,
-                                      downloadNow, uploadTotal, uploadNow);
-    return true;
-  };
+  // auto onProgress = [handler](cpr_off_t downloadTotal, cpr_off_t downloadNow,
+  //                             cpr_off_t uploadTotal, cpr_off_t uploadNow,
+  //                             intptr_t userdata) -> bool {
+  //   handler.instance->ExecuteFunction(CName("OnProgress"), downloadTotal,
+  //                                     downloadNow, uploadTotal, uploadNow);
+  //   return true;
+  // };
 
-  if (method == "GET") {
-    AsyncResponse fr =
-        GetAsync(Url{url.c_str()}, WriteCallback(onWrite),
-                 HeaderCallback(onHeader), ProgressCallback(onProgress));
-  }
+  // if (method == "GET") {
+  //   std::ofstream of("1.jpg", std::ios::binary);
+  //   cpr::Response r =
+  //       cpr::Download(of, cpr::Url{"http://www.httpbin.org/1.jpg"});
+
+  //   AsyncResponse fr =
+  //       GetAsync(Url{url.c_str()}, WriteCallback(onWrite),
+  //                HeaderCallback(onHeader), ProgressCallback(onProgress));
+  // }
 }
 
 void RegisterTypes() {
